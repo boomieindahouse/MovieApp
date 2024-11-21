8a29830eae3d22e29c9fa3c9f9e3a854e2a515e1
boomieindahouse <prawitsanat@hotmail.com>
2024-11-21 13:18:55 +0700

    add font

--- App.js
+++ App.js
@@ -3,14 +3,24 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
 
-
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="What should I ดู" component={HomeScreen} />
+      <Stack.Navigator 
+        initialRouteName="Home" 
+        screenOptions={{
+          headerStyle: { backgroundColor: '#6200EE' },
+          headerTintColor: '#fff',
+          headerTitleStyle: { fontFamily: 'Poppins_600SemiBold', fontSize: 20 },
+        }}
+      >
+        <Stack.Screen 
+          name="What should I ดู" 
+          component={HomeScreen} 
+          options={{ title: 'What Should I ดู' }} // ตั้งชื่อ Header
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
