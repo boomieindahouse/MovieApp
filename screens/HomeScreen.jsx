5127e465a2e87a0c2c7149484d380f8d7f215dbb
boomieindahouse <118826310+boomieindahouse@users.noreply.github.com>
2025-12-02 09:54:13 +0700

    feat: Implement `RNPickerSelect` for genre selection and update project dependencies.

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,109 +1,142 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, ActivityIndicator } from 'react-native';
-import axios from 'axios';
-import MovieCard from '../components/MovieCard';
-import { styles } from '../styles/styles';
-import { API_KEY, BASE_URL } from '../utils/constants';
-import globalStyles from '../globalStyles';
-import ModalDropdown from 'react-native-modal-dropdown';
+import React, { useState, useEffect } from "react";
+import { View, Text, FlatList, ActivityIndicator } from "react-native";
+import axios from "axios";
+import MovieCard from "../components/MovieCard";
+import { styles } from "../styles/styles";
+import { API_KEY, BASE_URL, ROUTES } from "../utils/constants";
+import globalStyles from "../globalStyles";
+import RNPickerSelect from "react-native-picker-select";
+import { Ionicons } from "@expo/vector-icons";
 
 const HomeScreen = ({ navigation }) => {
-    const [popularMovies, setPopularMovies] = useState([]);
-    const [genres, setGenres] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [genres, setGenres] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    // Fetch genres from the API
-    useEffect(() => {
-        axios
-            .get(`${BASE_URL}/genre/movie/list?api_key=${API_KEY}&language=en-US`)
-            .then((response) => {
-                setGenres(response.data.genres);
-            })
-            .catch((err) => {
-                setError(err);
-            });
-    }, []);
+  // Fetch genres from the API
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/genre/movie/list?api_key=${API_KEY}&language=en-US`)
+      .then((response) => {
+        setGenres(response.data.genres);
+      })
+      .catch((err) => {
+        setError(err);
+      });
+  }, []);
 
-    // Fetch popular movies
-    useEffect(() => {
-        setIsLoading(true);
-        const url = `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US`;
+  // Fetch popular movies
+  useEffect(() => {
+    setIsLoading(true);
+    const url = `${BASE_URL}/movie/popular?api_key=${API_KEY}&language=en-US`;
 
-        axios
-            .get(url)
-            .then((response) => {
-                setPopularMovies(response.data.results);
-                setIsLoading(false);
-            })
-            .catch((err) => {
-                setError(err);
-                setIsLoading(false);
-            });
-    }, []);
+    axios
+      .get(url)
+      .then((response) => {
+        setPopularMovies(response.data.results);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        setIsLoading(false);
+      });
+  }, []);
 
-
-    if (isLoading) {
-        return (
-            <View style={styles.centered}>
-                <ActivityIndicator size="large" color="#0000ff" />
-            </View>
-        );
-    }
-
-    if (error) {
-        return (
-            <View style={styles.centered}>
-                <Text>Error: {error.message}</Text>
-            </View>
-        );
-    }
-
+  if (isLoading) {
     return (
-        <View style={styles.container}>
-
-            <Text style={globalStyles.headerText}>Select Genre</Text>
-            {/* Dropdown for Genre selection */}
-            <ModalDropdown
-                options={['All Movies', ...genres.map((genre) => genre.name)]} // Display genres in dropdown
-                onSelect={(index, value) => {
-                    const genreId = genres.find((genre) => genre.name === value)?.id;
-                    if (genreId) {
-                        // Navigate to GenreMovieScreen and pass the genre ID
-                        navigation.navigate('GenreMovieScreen', {
-                            genreId: genreId,
-                            genreName: value,
-                        });
-                    } else {
-                        // If 'All Movies' is selected, stay on the current screen
-                        navigation.navigate('HomeScreen');
-                    }
-                }}
-                style={styles.dropdownStyle}
-                textStyle={styles.dropdownTextStyle}
-                dropdownStyle={styles.modalDropdown}
-            />
-            <Text style={globalStyles.headerText}>Popular Movies</Text>
-
-            <FlatList
-                data={popularMovies}
-                renderItem={({ item }) => (
-                    <MovieCard
-                        movie={item}
-                        onPress={() =>
-                            navigation.navigate('MovieDetail', {
-                                movieId: item.id,
-                                movieTitle: item.title,
-                            })
-                        }
-                    />
-                )}
-                keyExtractor={(item) => item.id.toString()} // จัดการ key ในระดับ FlatList
-                numColumns={2}
-                columnWrapperStyle={styles.row}
-            />
-        </View>
+      <View style={styles.centered}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
     );
+  }
+
+  if (error) {
+    return (
+      <View style={styles.centered}>
+        <Text>Error: {error.message}</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={globalStyles.headerText}>Select Genre</Text>
+      {/* Dropdown for Genre selection */}
+      <RNPickerSelect
+        onValueChange={(value) => {
+          if (value) {
+            const genre = genres.find((g) => g.id === value);
+            navigation.navigate(ROUTES.GENRE, {
+              genreId: value,
+              genreName: genre ? genre.name : "",
+            });
+          } else {
+            navigation.navigate(ROUTES.HOME);
+          }
+        }}
+        value={null}
+        items={[
+          { label: "All Movies", value: null },
+          ...genres.map((genre) => ({ label: genre.name, value: genre.id })),
+        ]}
+        useNativeAndroidPickerStyle={false}
+        Icon={() => {
+          return <Ionicons name="chevron-down" size={24} color="gray" />;
+        }}
+        style={{
+          inputIOS: {
+            height: 50, // Increased height
+            borderWidth: 1,
+            borderColor: "#ccc",
+            borderRadius: 5,
+            paddingHorizontal: 10,
+            marginBottom: 20,
+            textAlign: "center",
+            color: "black",
+            width: "100%", // Ensure full width
+            justifyContent: "center",
+          },
+          inputAndroid: {
+            height: 50, // Increased height
+            borderWidth: 1,
+            borderColor: "#ccc",
+            borderRadius: 5,
+            paddingHorizontal: 10,
+            marginBottom: 20,
+            textAlign: "center",
+            color: "black",
+            width: "100%", // Ensure full width
+            justifyContent: "center",
+          },
+          iconContainer: {
+            top: 12,
+            right: 15,
+          },
+        }}
+        placeholder={{}}
+      />
+      <Text style={globalStyles.headerText}>Popular Movies</Text>
+
+      <FlatList
+        data={popularMovies}
+        renderItem={({ item }) => (
+          <MovieCard
+            movie={item}
+            onPress={() =>
+              navigation.navigate("MovieDetail", {
+                movieId: item.id,
+                movieTitle: item.title,
+              })
+            }
+          />
+        )}
+        keyExtractor={(item) => item.id.toString()} // จัดการ key ในระดับ FlatList
+        numColumns={2}
+        columnWrapperStyle={styles.row}
+      />
+    </View>
+  );
 };
 
 export default HomeScreen;
